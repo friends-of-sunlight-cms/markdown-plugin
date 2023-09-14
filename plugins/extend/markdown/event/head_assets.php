ef677c95e5999596e785fc3e06217e84ae325de7
jDanek <jirka.danek@jdanek.eu>
2023-09-14 19:37:37 +0200

    Remove darkmode autodetect

--- plugins/extend/markdown/event/head_assets.php
+++ plugins/extend/markdown/event/head_assets.php
@@ -7,14 +7,5 @@ use Sunlight\Template;
 return function (array $args) {
     $config = $this->getConfig();
     $dark = $config['dark_mode'];
-
-    if ($dark === null) {
-        if (Core::$env === Core::ENV_ADMIN) {
-            $dark = Settings::get('adminscheme_dark');
-        } else {
-            $dark = Template::getCurrent()->getOption('dark');
-        }
-    }
-
     $args['css'][] = $this->getAssetPath('public/css/markdown-' . ($dark ? 'dark' : 'light') . '.css');
 };
